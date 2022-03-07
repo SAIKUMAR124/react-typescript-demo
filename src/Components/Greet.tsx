519ff9334c0544dd8087175143472d25c642ba5d
dev global <pavankumar.s12459@gmail.com>
2022-03-07 18:51:18 +0530

    Add message Count

--- src/Components/Greet.tsx
+++ src/Components/Greet.tsx
@@ -1,11 +1,12 @@
 type GreetProps = {
   name: string;
+  messageCount: number;
 };
 
 const Greet = (props: GreetProps) => {
   return (
     <div>
-      <h2>Welcome {props.name}! You have 10 unread messages</h2>
+      <h2>Welcome {props.name}! You have {props.messageCount} unread messages</h2>
     </div>
   );
 };
