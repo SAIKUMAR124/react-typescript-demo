cd8dba27afcf3712d2b5475b89d0f8de387681f6
dev global <pavankumar.s12459@gmail.com>
2022-03-07 19:09:04 +0530

    Object Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import "./App.css";
 import Greet from "./Components/Greet";
+import { Person } from "./Components/Person";
 
 function App() {
+
+  const personName = {
+    first: "Bruce",
+    last: "Wayne",
+  };
+
   return (
     <div className="App">
       <Greet name="Pavan" messageCount={11} isLoggedIn={true} />
+      <Person name={personName} />
     </div>
   );
 }
