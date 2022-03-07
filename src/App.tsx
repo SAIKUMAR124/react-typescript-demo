5365dcfc71dce4d1458ae8ca4f45dea674643bb7
dev global <pavankumar.s12459@gmail.com>
2022-03-07 18:48:32 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Greet from "./Components/Greet";
+
+function App() {
+  return (
+    <div className="App">
+      <Greet name="Pavan" />
+    </div>
+  );
+}
+
+export default App;
