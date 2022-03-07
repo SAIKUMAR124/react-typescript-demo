58eaab855c9ea2f2b7a25852a5ab90d6e630f0f1
dev global <pavankumar.s12459@gmail.com>
2022-03-07 18:57:50 +0530

    Added Boolean type

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Greet from "./Components/Greet";
 function App() {
   return (
     <div className="App">
-      <Greet name="Pavan" messageCount={11} />
+      <Greet name="Pavan" messageCount={11} isLoggedIn={true} />
     </div>
   );
 }
