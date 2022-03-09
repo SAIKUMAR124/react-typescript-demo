cdc20606d1d4aec3bd2b5b47c1173a960d49ef79
dev global <pavankumar.s12459@gmail.com>
2022-03-09 10:32:43 +0530

    Types in .ts file and Reusability

--- src/Components/Person.tsx
+++ src/Components/Person.tsx
@@ -1,13 +1,9 @@
-// Object Props
-type PersonProps = {
-    name: {
-        first: string;
-        last: string;
-    }
-}
+import { PersonProps } from "./Person.types";
 
 export const Person = (props: PersonProps) => {
   return (
-    <div>{props.name.first} {props.name.last}</div>
-  )
-}
+    <div>
+      {props.name.first} {props.name.last}
+    </div>
+  );
+};
